c888650d5033e96a70a77e1c8b317e3dea477dee
Rinat <pandar1n991@gmail.com>
2023-01-25 20:35:20 +0300

    Готов

---
+++ src/test/java/Home_work_3/dop/tablicaUmnozheniya.java
@@ -0,0 +1,22 @@
+package Home_work_3.dop;
+
+public class tablicaUmnozheniya {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10 ; i++) {
+            for (int j = 1; j <= 10; j++) {
+
+                System.out.print(i * j + " ");
+
+            }
+            System.out.println( "  ");
+
+
+        }
+    }
+}
+
+
+
+
+
---
+++ src/test/java/Home_work_3/dop/treugonik.java
@@ -0,0 +1,13 @@
+package Home_work_3.dop;
+
+public class treugonik {
+    public static void main(String[] args) {
+        String znak = "^";
+        for (int i = 0; i < 10; i++) {
+            System.out.println(znak);
+            znak = znak + "^";
+        }
+
+    }
+}
+
